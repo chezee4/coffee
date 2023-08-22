577d80ef2ba4c47b6d27a3746d4d20fa616ba855
chezee <vadimdufinec@gmail.com>
2023-08-22 07:38:50 +0300

    optimization

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -3,7 +3,7 @@ import {FC} from 'react';
 import SpinnerLoading from "../../assets/img/Spinner.gif";
 const Spinner: FC = () => {
     return (
-        <img src={SpinnerLoading} alt="Spinner" className="absolute top-0 left-0 right-0 bottom-0 "/>
+        <img src={SpinnerLoading} alt="Spinner" className=" absolute z-10 w-full h-full"/>
     );
 };
 

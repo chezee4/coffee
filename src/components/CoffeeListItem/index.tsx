c307263a34e69aa4a8f2af0e8f0d4fefacc6814b
chezee <vadimdufinec@gmail.com>
2023-08-22 05:47:20 +0300

    fix animation CoffeListItem

--- src/components/CoffeeListItem/index.tsx
+++ src/components/CoffeeListItem/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
@@ -10,16 +10,26 @@ interface CoffeeListItemProps {
 }
 const CoffeeListItem: FC<CoffeeListItemProps> = ({ product, index }) => {
   const { title, img, alt, price, country } = product;
+  const [hover, setHover] = useState(true);
   return (
     <motion.li
       initial={{ opacity: 0, scale: 0.8 }}
-      whileInView={{ opacity: 1, scale: 1 }}
-      transition={{
-        duration: 1,
-        delay: 0.2 * index,
-        ease: "linear",
+      whileHover={{
+        scale: 1.1,
+        transition:{ duration: 0.35, delay: 0 }
       }}
-      className=" list-none rounded-lg bg-white p-4 text-right cursor-pointer shadow-customShadow transition-all duration-300  ease-linear hover:scale-105 "
+      whileInView={{
+        opacity: 1,
+        scale: 1,
+        transition: {
+          duration: hover? 0.8 : 0.35,
+          delay: hover ? 0.2 * index : 0,
+          ease: "linear",
+        },
+      }}
+      onHoverStart={() => setHover(false)}
+      onHoverEnd={() => setHover(true)}
+      className=" list-none rounded-lg bg-white p-4 text-right cursor-pointer shadow-customShadow"
     >
       <Link to={`/product/${product.id}`}>
         <img
